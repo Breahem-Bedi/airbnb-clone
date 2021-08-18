58352ef931eeb6acad35d95537ec8beb1b4612ff
Breahem-Bedi <lovelybedi@hotmail.com>
2021-08-19 02:21:04 +1200

    Search Page - Server Side Rendering Added

--- pages/search.js
+++ pages/search.js
@@ -2,8 +2,10 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { useRouter } from "next/dist/client/router";
 import { format } from "date-fns";
+import InfoCard from "../components/InfoCard";
 
-function Search() {
+function Search({ searchResults }) {
+    console.log("Results", searchResults);
 
     const router = useRouter();
     const { location, startDate, endDate, numGuests } = router.query;
@@ -25,6 +27,21 @@ function Search() {
                         <p className="button">Rooms and Beds</p>
                         <p className="button">More Filters</p>
                     </div>
+                    <div className="flex flex-col">
+                            {searchResults?.map(({ img, location, title, description, star, price, total }) => (
+                                <InfoCard
+                                    key={img} 
+                                    img={img}
+                                    location={location}
+                                    title={title}
+                                    description={description}
+                                    star={star}
+                                    price={price}
+                                    total={total}
+
+                                />
+                            ))}
+                        </div>
                 </section>
 
             </main>
@@ -34,3 +51,13 @@ function Search() {
 }
 
 export default Search;
+
+export async function getServerSideProps(){
+    const searchResults = await fetch("https://links.papareact.com/isz").then(res => res.json());
+
+    return {
+        props: {
+            searchResults,
+        }
+    }
+}
